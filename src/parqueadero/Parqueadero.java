f519e23e859883e0be0c93f0a46254a6345da985
AndresFMH <anfelmh06@gmail.com>
2024-02-09 20:01:14 -0500

    Se agrega el proyecto

---
+++ src/parqueadero/Parqueadero.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parqueadero;
+
+/**
+ *
+ * @author andre
+ */
+public class Parqueadero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana v = new Ventana();
+        v.setTitle("Parqueadero");
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+        
+}
---
+++ src/parqueadero/Vehiculos.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package parqueadero;
+
+/**
+ *
+ * @author andre
+ */public class Vehiculos {
+    
+    private String placa, tipo, estado;
+
+    public Vehiculos(String placa, String tipo) {
+        this.placa = placa;
+        this.tipo = tipo;
+        this.estado = "Ingresado";
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
+
